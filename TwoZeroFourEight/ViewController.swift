a9a9255c94b2f731d7dbc72a33baa3513bacc62d
steveeri <steveeri@gmail.com>
2018-06-26 23:52:58 +1000

    storyboard changes for rudimentary game square

---
+++ TwoZeroFourEight/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  TwoZeroFourEight
+//
+//  Created by Steve Richards on 26/06/2018.
+//  Copyright © 2018 Steve Richards. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
