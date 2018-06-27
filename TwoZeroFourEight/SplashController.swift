ba0c2b96a9863c42a18939dc1adb2b71f08d9d28
steveeri <steveeri@gmail.com>
2018-06-28 00:40:40 +1000

    add splash page

--- TwoZeroFourEight/SplashController.swift
+++ TwoZeroFourEight/SplashController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class SplashController: UIViewController {
 
+    
+    @IBAction func playGameTapped(sender: SSRoundedButton) {
+        print(sender.currentTitle)
+    }
+    
+    
+    @IBAction func quitGameTapped(sender: SSRoundedButton) {
+        print(sender.currentTitle)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,16 +31,5 @@ class SplashController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
